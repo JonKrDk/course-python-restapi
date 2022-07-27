f1029158ef29d5a166e7424c9c7ba83680605a11
Jon Kristensen <jon@jonkr.dk>
2022-07-27 21:10:23 +0200

    Fixed typos after running codechecker

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -24,9 +24,9 @@ class ModelTests(TestCase):
         """Test email is normalized for new users."""
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2@example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
-            ['test4@EXAMPLE.COM', 'test4@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
