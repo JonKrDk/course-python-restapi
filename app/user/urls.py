e57c54a08bcaf46c2c9542886c4e4fd0e8bd152f
Jon Kristensen <jon@jonkr.dk>
2022-08-12 19:53:54 +0200

    Fixed a strange import error in app/user/views.py

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,13 +1,13 @@
 """
 URL mappings for the user API.
 """
-# from django.urls import path
-#
-# from . import views
-#
-#
-# app_name = 'user'
-#
-# urlpatterns = [
-#     path('create/', views.CreateUserView.as_view(), name='create'),
-# ]
+from django.urls import path
+
+from . import views
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+]
